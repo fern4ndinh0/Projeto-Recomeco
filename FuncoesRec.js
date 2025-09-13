603a1948d3b24490fd4e89a2b5ce6e6208441f1e
Fernando Dias <di4s.fernando@gmail.com>
2025-09-12 21:55:19 -0300

    removendo arquivos antigos

--- FuncoesRec.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.bt-saiba').addEventListener('click', e => {
-  e.preventDefault();
-  document
-    .getElementById('contato')
-    .scrollIntoView({ behavior: 'smooth' });
-});
